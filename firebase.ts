87726ca554e9f97fb5afdbf7990b057b5100c970
Rickard <dahlstrom.rickard@gmail.com>
2025-03-23 14:10:24 +0100

    Fix ts errors

--- firebase.ts
+++ firebase.ts
@@ -18,6 +18,20 @@ const getFirebaseConfig = () => {
     // Production configuration
     return {
       // These are currently set in gitHub -> settings (for the repo) -> secrets and variables -> actions
+      // @ts-expect-error temporary
+      apiKey: secret.FIREBASE_API_KEY,
+      // @ts-expect-error temporary
+      authDomain: secret.FIREBASE_AUTH_DOMAIN,
+      // @ts-expect-error temporary
+      projectId: secret.FIREBASE_PROJECT_ID,
+      // @ts-expect-error temporary
+      storageBucket: secret.FIREBASE_STORAGE_BUCKET,
+      // @ts-expect-error temporary
+      messagingSenderId: secret.FIREBASE_MESSAGING_SENDER_ID,
+      // @ts-expect-error temporary
+      appId: secret.FIREBASE_APP_ID,
+      // @ts-expect-error temporary
+      measurementId: secret.FIREBASE_MEASUREMENT_ID,
     };
   }
 };
