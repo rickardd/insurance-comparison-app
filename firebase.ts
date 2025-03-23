dcc78d71a4a7bbf9456600eed724e3aa25ba80c4
Rickard <dahlstrom.rickard@gmail.com>
2025-03-23 19:22:52 +0100

    Test gitHub actions

--- firebase.ts
+++ firebase.ts
@@ -18,20 +18,13 @@ const getFirebaseConfig = () => {
     // Production configuration
     return {
       // These are currently set in gitHub -> settings (for the repo) -> secrets and variables -> actions
-      // @ts-expect-error temporary
-      apiKey: secret.FIREBASE_API_KEY,
-      // @ts-expect-error temporary
-      authDomain: secret.FIREBASE_AUTH_DOMAIN,
-      // @ts-expect-error temporary
-      projectId: secret.FIREBASE_PROJECT_ID,
-      // @ts-expect-error temporary
-      storageBucket: secret.FIREBASE_STORAGE_BUCKET,
-      // @ts-expect-error temporary
-      messagingSenderId: secret.FIREBASE_MESSAGING_SENDER_ID,
-      // @ts-expect-error temporary
-      appId: secret.FIREBASE_APP_ID,
-      // @ts-expect-error temporary
-      measurementId: secret.FIREBASE_MEASUREMENT_ID,
+      apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+      authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+      projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+      storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+      appId: import.meta.env.VITE_FIREBASE_APP_ID,
+      measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
     };
   }
 };
